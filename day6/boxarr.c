ea05ebef36b587115f525e953f246c6d0d6a195b
Haripriyathota26 <haripriyanaidu124@gmail.com>
2023-06-22 17:48:47 +0530

    sol

---
+++ day6/boxarr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct bitsox {
+    double l;
+    double h;
+    double w;
+};
+
+void Box_a_v(struct Box *box) {
+    double vol = box->l * box->w * box->h;
+    double surfaceArea = 2 * (box->l * box->w + box->l * box->h + box->w * box->h);
+    
+    printf("Volume: %.2f\n", vol);
+    printf("Surface Area: %.2f\n", surfaceArea);
+}
+
+int main() {
+    struct Box myBox;
+    struct Box *Ptr = &myBox;
+
+    printf("Enter the length of the box: ");
+    scanf("%lf", &Ptr->l);
+    
+    printf("Enter the width of the box: ");
+    scanf("%lf", &Ptr->w);
+    
+    printf("Enter the height of the box: ");
+    scanf("%lf", &Ptr->h);
+
+    Box_a_v(Ptr);
+
+    return 0;
+}
